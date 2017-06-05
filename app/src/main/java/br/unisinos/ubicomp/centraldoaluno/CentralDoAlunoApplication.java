78a5087c806539e64acec80d91ab75d09fa4cece
Matias Schertel <mschertel@gmail.com>
2017-06-04 22:51:28 -0300

    add basic beacon code

---
+++ app/src/main/java/br/unisinos/ubicomp/centraldoaluno/CentralDoAlunoApplication.java
@@ -0,0 +1,66 @@
+package br.unisinos.ubicomp.centraldoaluno;
+
+import android.app.Application;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+import java.util.UUID;
+
+public class CentralDoAlunoApplication extends Application implements BootstrapNotifier {
+    private static String TAG = CentralDoAlunoApplication.class.getSimpleName();
+
+    private RegionBootstrap regionBootstrap;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        initializeValues();
+
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        Region region = new Region("br.unisinos.ubicomp.boostrapRegion", null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+    }
+
+    private void initializeValues() {
+        String deviceName = Build.MODEL;
+        String deviceId = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+        if (deviceId == null || deviceId.isEmpty()) {
+            deviceId = UUID.randomUUID().toString();
+        }
+
+        PreferencesAdapter pa = PreferencesAdapter.getInstance(this);
+
+        if (pa.getUserName() == null || pa.getUserName().isEmpty()) {
+            pa.setUserName(deviceName);
+        }
+        if (pa.getDeviceId() == null || pa.getDeviceId().isEmpty()) {
+            pa.setDeviceId(deviceId);
+        }
+    }
+
+    @Override
+    public void didEnterRegion(Region region) {
+        Log.d(TAG, String.format("didEnterRegion(%1s)", region));
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        Log.d(TAG, String.format("didExitRegion(%1s)", region));
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+        Log.d(TAG, String.format("didDetermineStateForRegion(%1s, %2s)", i, region));
+    }
+}
